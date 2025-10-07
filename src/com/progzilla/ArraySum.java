1fe91fe30e874823e0fd2e290c7188e4ead20374
Ola Popoola <oladipupo.popoola@gmail.com>
2025-10-07 23:36:11 +0100

    solved ArraySum for loop

--- src/com/progzilla/ArraySum.java
+++ src/com/progzilla/ArraySum.java
@@ -5,5 +5,20 @@ public class ArraySum {
         /* Write a program that calculates the sum of all elements
         in an array using an enhanced for loop.
          */
+
+        // declare and initialise an array
+        int[] arraySum = {3,4,5,2,7,6};
+        int total_array = 0;
+
+        // standard for loop
+        for (int i=0; i < arraySum.length; i++){
+            // calculate
+            total_array += arraySum[i]; // total_array = total_array + arraySum[i]
+        }
+        // enhanced for loop
+        for (int arrayNumber : arraySum) {
+            total_array += arrayNumber;
+        }
+        System.out.println("Total Sum of Array is: " + total_array);
     }
 }
