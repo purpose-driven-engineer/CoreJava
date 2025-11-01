c20ac8cde56b38b4fbeb6592746fcc45c4a6261a
Ola Popoola <oladipupo.popoola@gmail.com>
2025-11-01 22:51:53 +0000

    adjusted and trying enhanced loop

--- src/com/progzilla/ReverseString.java
+++ src/com/progzilla/ReverseString.java
@@ -1,22 +1,27 @@
 package com.progzilla;
 
+import java.util.Arrays;
+
 public class ReverseString {
     public static void main(String[] args){
-        int[] anArray = new int[10];
-        String[] names = {"ola", "benny", "tayo", "jake"};
-        double[] prices = {23.33, 2.12, 0.55};
+
+        /*
+        Copy an array of numbers into empty array
+         */
+        //... still in progress
 
         int[] copyFrom = {1,2,3,4,5,6};
         int[] copyTo = {copyFrom.length};
 
-//        for (int i = 0; i > copyFrom.length; i++){
-//            copyFrom[i] = copyTo[i];
-//        }
+        for (int i = 0; i > copyFrom.length; i++){
+            copyFrom[i] = copyTo[i];
+        }
 
+        // play with enhanced loop
         for (String name : names){
             System.out.println(name);
         }
 
-        //System.out.println(Arrays.toString(copyTo));
+        System.out.println(Arrays.toString(copyTo));
     }
 }
