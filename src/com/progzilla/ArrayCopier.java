7ad04beb5cee4755886bca7d224414b8dc9b40bc
Ola Popoola <oladipupo.popoola@gmail.com>
2025-11-02 01:00:05 +0000

    output issues resolved

--- src/com/progzilla/ArrayCopier.java
+++ src/com/progzilla/ArrayCopier.java
@@ -10,7 +10,6 @@ public class ArrayCopier {
         /*
         Copy an array of numbers into empty array
          */
-        //... still in progress
 
         int[] copyFrom = {1, 2, 3, 4, 5, 6};
         int[] copyTo = new int[copyFrom.length];
@@ -18,13 +17,14 @@ public class ArrayCopier {
         // beginner-level method: for loop
         for (int i = 0; i < copyTo.length; i++)
                 copyTo[i] = copyFrom[i];
+        System.out.println(Arrays.toString(copyTo));
 
         // concise method: copyOf
         int[] copyToArray = Arrays.copyOf(copyFrom, 6);
+        System.out.println(Arrays.toString(copyToArray));
 
         // efficient method: arraycopy
         System.arraycopy(copyFrom, 0, copyTo, 0, copyTo.length);
-
-        System.out.println(Arrays.toString(copyToArray));
+        System.out.println(Arrays.toString(copyTo));
     }
 }
